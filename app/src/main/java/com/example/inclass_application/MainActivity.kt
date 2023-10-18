ae48debdbac4481f992d816fba1c8f9815217e6b
CarefreeLife <csm4903@naver.com>
2023-10-18 15:46:19 +0900

    update codes

---
+++ app/src/main/java/com/example/inclass_application/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inclass_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.geojae_river.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/inclassapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inclassapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.geojae_river.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
